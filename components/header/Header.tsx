88a62f1253f5a8607437bb4cab1b67445d1f995d
Zohaib Akram <za@justad.ai>
2025-12-16 01:06:01 +0500

    fix:fix header button UI

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -6,12 +6,11 @@ import { Button } from "../ui/button"
 
 
 export const Header = () => {
-
     return <header className="max-w-345 m-auto py-5 lg:py-11  2xl:px-0 flex items-center px-6 lg:px-10 justify-between">
         <Logo />
         <Navigation />
         <div className="flex justify-center items-center gap-2.5">
-            <Button variant={"outline"} className="bg-transparent h-10 text-white cursor-pointer">Get in touch</Button>
+            <Button variant={"outline"} className="bg-transparent h-10 text-[#FFF] font-medium text-base rounded-xl cursor-pointer">Get in touch</Button>
             <MobileNavigation />
         </div>
     </header>
